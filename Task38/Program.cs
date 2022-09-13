182101dc040410cba270ecd67876596e39ea6e93
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-14 02:54:39 +0300

    Task38  - решение испр.

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -3,38 +3,38 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-void FillArray(int[] array)     // заполнение массива случайными числами из диапазона [-100;100]
+void FillArray(double[] array)
 {
     Random rnd = new Random();
+    int rndIndex = 10;
     int length = array.Length;
-    int minNum = -100, maxNum = 101;
     for (int i = 0; i < length; i++)
     {
-        array[i] = rnd.Next(minNum, maxNum);
+        array[i] = Math.Round(rnd.NextDouble() * rndIndex, 2, MidpointRounding.ToZero);
     }
 }
 
-void CountDiffMaxMinArray(int[] array)
+void CountDiffMaxMinArray(double[] array)
 {
     int length = array.Length;
-    int minNum = array[0];
-    int maxNum = array[0];
+    double minNum = array[0];
+    double maxNum = array[0];
     string arrayOutput = String.Empty;
     for (int i = 0; i < length; i++)
     {
         if (array[i] < minNum) minNum = array[i];
         if (array[i] > maxNum) maxNum = array[i];
-        if(i == length - 1) arrayOutput += array[i];
+        if (i == length - 1) arrayOutput += array[i];
         else arrayOutput += $"{array[i]} ";
     }
-    int diffNum = maxNum - minNum;
+    double diffNum = Math.Round(maxNum - minNum, 2, MidpointRounding.ToZero);
     Console.WriteLine($"Разница между максимальным и минимальным элементами массива [{arrayOutput}] -> {diffNum}");
 }
 
 Console.Write("Введите кол-во элементов массива: ");
 int arrayNum = Convert.ToInt32(Console.ReadLine());
 
-int[] arr = new int[arrayNum];
+double[] arr = new double[arrayNum];
 
 FillArray(arr);
 CountDiffMaxMinArray(arr);
