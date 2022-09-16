784b417e9fbdfcc957f52fc888c3329719428ce5
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-16 10:33:45 +0300

    решение испр.

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,20 +1,44 @@
 ﻿// Задача 38:
 // Задайте массив вещественных чисел.
 // Найдите разницу между максимальным и минимальным элементов массива.
-// [3 7 22 2 78] -> 76
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
-void FillArray(double[] array)
+double[] CreateArray(int size, int range)
 {
+    double[] array = new double[size];
     Random rnd = new Random();
-    int rndIndex = 10;
-    int length = array.Length;
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < size; i++)
     {
-        array[i] = Math.Round(rnd.NextDouble() * rndIndex, 2, MidpointRounding.ToZero);
+        array[i] = Math.Round(rnd.NextDouble() * range, 2, MidpointRounding.ToZero);
     }
+    return array;
 }
 
-void CountDiffMaxMinArray(double[] array)
+// void PrintArray(double[] array)    // выводит список элементов в консоль
+// {
+//     int length = array.Length;
+//     Console.Write("[");
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(i== length - 1) Console.Write(array[i]);
+//         else Console.Write($"{array[i]}, ");
+//     }
+//     Console.WriteLine("]");
+// }
+
+string PrintArrayAsString(double[] array)  // возвращает строку с элементами массива
+{
+    int length = array.Length;
+    string output = String.Empty;
+    for (int i = 0; i < length; i++)
+    {
+        if (i == length - 1) output += array[i];
+        else output += $"{array[i]}, ";
+    }
+    return output;
+}
+
+double CountDiffMaxMinArray(double[] array) // возвращает разницу между макс. и мин. значениями массива
 {
     int length = array.Length;
     double minNum = array[0];
@@ -24,17 +48,21 @@ void CountDiffMaxMinArray(double[] array)
     {
         if (array[i] < minNum) minNum = array[i];
         if (array[i] > maxNum) maxNum = array[i];
-        if (i == length - 1) arrayOutput += array[i];
-        else arrayOutput += $"{array[i]} ";
     }
     double diffNum = Math.Round(maxNum - minNum, 2, MidpointRounding.ToZero);
-    Console.WriteLine($"Разница между максимальным и минимальным элементами массива [{arrayOutput}] -> {diffNum}");
+    return diffNum;
 }
 
+// данные для создания массива
 Console.Write("Введите кол-во элементов массива: ");
-int arrayNum = Convert.ToInt32(Console.ReadLine());
+int arrayNums = Convert.ToInt32(Console.ReadLine());
 
-double[] arr = new double[arrayNum];
+// результат создания массива
+double[] arr = CreateArray(arrayNums, 10);  // массив в диапазоне [0.0,10.0]
+string initArrayOut = PrintArrayAsString(arr);
 
-FillArray(arr);
-CountDiffMaxMinArray(arr);
+// разница между макс. и мин. значениями в массиве
+double diffMaxMin = CountDiffMaxMinArray(arr);
+
+// вывод консоль
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива [{initArrayOut}] -> {diffMaxMin}");
